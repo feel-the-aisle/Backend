9faa0efd2769cfec6baa892eac8edc6ff88f4678
yool <syhj3238@naver.com>
2024-07-26 18:36:38 +0900

    Refactor code pathfinding

--- find_path/service/find_service.py
+++ find_path/service/find_service.py
@@ -1,7 +1,5 @@
-import sqlite3
-import json
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from db_model.mysql import db, ConvenienceStoreInfo, ConvenienceStoreMap
 
 
 
@@ -22,255 +20,261 @@ class Node:
   def __eq__(self, other):
     return self.position == other.position
 
-
-# 상하좌우 가중치
-def cost_check(node, goal, D=1):  # 상하좌우에 대한 가중치
-  # node: 현재 노드, position- 현재 노드의 위치를 지칭하는 것
-  # goal: 목표 노드(도착 지점)
-  # D: 상하좌우 이동에 대한 비용계산. = 1
-  left_right = abs(node.position[0] - goal.position[0])
-  top_bottom = abs(node.position[1] - goal.position[1])
-  return D * (left_right + top_bottom)
-
-
-# 경로 찾기
-def find_shortest_path(maze, start, end):
-  # 시작/ 도착 지점 초기화
-  startNode = Node(None, start)
-  endNode = Node(None, end)
-
-  # 저장 경로 초기화(reserve = 계산 중 / final = 결정된 것)
-  reserveList = []
-  finalList = []
-
-  # reserveList에 시작 노드 추가
-  reserveList.append(startNode)
-
-  # 도착 지점을 찾을 때까지 실행 ( endNode 찾기까지 샐행 )
-  while reserveList:
-
-    # 현재 노드 지정
-    currentNode = reserveList[0]
-    currentIdx = 0
-
-    # 이미 같은 노드가 reserveList에 있고, f 값이 더 크면
-    # currentNode를 reserveList안에 있는 값으로 교체
-    for index, item in enumerate(reserveList):
-      if item.f < currentNode.f:
-        currentNode = item
-        currentIdx = index
-
-    # reserveList에서 제거 후 finalList에 추가
-    reserveList.pop(currentIdx)
-    finalList.append(currentNode)
-
-    # 현재 노드가 목적지면 current.position 추가하고
-    # current의 부모로 이동
-    if currentNode == endNode:
-      path = []
-      current = currentNode
-      while current is not None:
-        path.append(current.position)
-        current = current.parent
-      return path[::-1]  # reverse
-
-    children = []
-
-    # 인접한 xy좌표 상하좌우
-    for newPosition in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
-
-      # 노드 위치 추가
-      nodePosition = (
-          currentNode.position[0] + newPosition[0],  # X
-          currentNode.position[1] + newPosition[1])  # Y
-
-      # maze index 범위 안에 있어야함
-      within_range_criteria = [
-          nodePosition[0] > (len(maze) - 1),
-          nodePosition[0] < 0,
-          nodePosition[1] > (len(maze[len(maze) - 1]) - 1),
-          nodePosition[1] < 0,
-      ]
-
-      # 하나라도 true면 범위 밖
-      if any(within_range_criteria):
-        continue
-
-      # 장애물 판단후 이동
-      # <진열대> 라면 : 1 / 음료 : 2 / 과자 : 3 / 기타 : 4
-      #          카운터 : 5 / 입구 : 6
-      if maze[nodePosition[0]][nodePosition[1]] in [1, 2, 3, 4, 5]:
-        continue
-
-      new_node = Node(currentNode, nodePosition)
-      children.append(new_node)
-
-    # 자식 loop
-    for child in children:
-
-      # 자식이 finalList에 있으면 진행
-      if child in finalList:
-        continue
-
-      # f, g, h값 업데이트
-      child.g = currentNode.g + 1
-      child.h = cost_check(child, endNode)  #코스트 계산 값
-      child.f = child.g + child.h
-
-      # 자식이 reserveList에 존재 && g 값이 더 크면 진행
-      if len([
-          openNode for openNode in reserveList
-          if child == openNode and child.g > openNode.g
-      ]) > 0:
-        continue
-
-      reserveList.append(child)
+class Detectservice:
+  
+  # 상하좌우 가중치
+  @staticmethod
+  def cost_check(node, goal, D=1):  # 상하좌우에 대한 가중치
+    # node: 현재 노드, position- 현재 노드의 위치를 지칭하는 것
+    # goal: 목표 노드(도착 지점)
+    # D: 상하좌우 이동에 대한 비용계산. = 1
+    left_right = abs(node.position[0] - goal.position[0])
+    top_bottom = abs(node.position[1] - goal.position[1])
+    return D * (left_right + top_bottom)
 
 
-# 지도의 가로, 세로 규격 전역 변수에 저장 (row: 가로, col: 세로)
-def get_maze_size(storeName):
-  storeN = ConvenienceStoreInfo.query.filter_by(storename = storeName).first()
-  row = storeN.storerow
-  col = storeN.storecol
-  result = [row, col]
-  return result
+  # 경로 찾기
+  @staticmethod
+  def find_shortest_path(maze, start, end):
+    # 시작/ 도착 지점 초기화
+    startNode = Node(None, start)
+    endNode = Node(None, end)
+
+    # 저장 경로 초기화(reserve = 계산 중 / final = 결정된 것)
+    reserveList = []
+    finalList = []
+
+    # reserveList에 시작 노드 추가
+    reserveList.append(startNode)
+
+    # 도착 지점을 찾을 때까지 실행 ( endNode 찾기까지 샐행 )
+    while reserveList:
+
+      # 현재 노드 지정
+      currentNode = reserveList[0]
+      currentIdx = 0
+
+      # 이미 같은 노드가 reserveList에 있고, f 값이 더 크면
+      # currentNode를 reserveList안에 있는 값으로 교체
+      for index, item in enumerate(reserveList):
+        if item.f < currentNode.f:
+          currentNode = item
+          currentIdx = index
+
+      # reserveList에서 제거 후 finalList에 추가
+      reserveList.pop(currentIdx)
+      finalList.append(currentNode)
+
+      # 현재 노드가 목적지면 current.position 추가하고
+      # current의 부모로 이동
+      if currentNode == endNode:
+        path = []
+        current = currentNode
+        while current is not None:
+          path.append(current.position)
+          current = current.parent
+        return path[::-1]  # reverse
+
+      children = []
+
+      # 인접한 xy좌표 상하좌우
+      for newPosition in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+
+        # 노드 위치 추가
+        nodePosition = (
+            currentNode.position[0] + newPosition[0],  # X
+            currentNode.position[1] + newPosition[1])  # Y
+
+        # maze index 범위 안에 있어야함
+        within_range_criteria = [
+            nodePosition[0] > (len(maze) - 1),
+            nodePosition[0] < 0,
+            nodePosition[1] > (len(maze[len(maze) - 1]) - 1),
+            nodePosition[1] < 0,
+        ]
+
+        # 하나라도 true면 범위 밖
+        if any(within_range_criteria):
+          continue
+
+        # 장애물 판단후 이동
+        # <진열대> 라면 : 1 / 음료 : 2 / 과자 : 3 / 기타 : 4
+        #          카운터 : 5 / 입구 : 6
+        if maze[nodePosition[0]][nodePosition[1]] in [1, 2, 3, 4, 5]:
+          continue
+
+        new_node = Node(currentNode, nodePosition)
+        children.append(new_node)
+
+      # 자식 loop
+      for child in children:
+
+        # 자식이 finalList에 있으면 진행
+        if child in finalList:
+          continue
+
+        # f, g, h값 업데이트
+        child.g = currentNode.g + 1
+        child.h = cost_check(child, endNode)  #코스트 계산 값
+        child.f = child.g + child.h
+
+        # 자식이 reserveList에 존재 && g 값이 더 크면 진행
+        if len([
+            openNode for openNode in reserveList
+            if child == openNode and child.g > openNode.g
+        ]) > 0:
+          continue
+
+        reserveList.append(child)
 
 
-# 지도 만들기
-def create_maze(storeName, galo, selo):
-  # 지도 초기 규격 만들기
-  storeId = ConvenienceStoreInfo.query.filter_by(storename=storeName).first()
-  ids = storeId.id
-
-  storeMaze = [[0] * galo for i in range(selo)]
-
-  storeMap = ConvenienceStoreInfo.query.filter_by(storeinfoid = ids).all()
-
-  for storeMap in storeMap:
-    row = storeMap.storex
-    col = storeMap.storey
-    sangte = storeMap.storestate
-
-    storeMaze[col][row] = sangte
-    
-  return storeMaze
+  # 지도의 가로, 세로 규격 전역 변수에 저장 (row: 가로, col: 세로)
+  @staticmethod
+  def get_maze_size(storeName):
+    storeN = ConvenienceStoreInfo.query.filter_by(storename = storeName).first()
+    row = storeN.storerow
+    col = storeN.storecol
+    result = [row, col]
+    return result
 
 
-# 출발, 도착 지점 가져오기 (편의점 이름, 지점 선택 번호(출,도 int))
-# 배열 RETURN -> 0: 출발 가로 / 1: 출발 세로 / 2: 도착 가로 / 3: 도착 세로
-def get_start_end(startP, endP, storeSelo, storeGalo, storeMaze):
-  strStart = startP
-  strEnd = endP
-  selo = storeSelo
-  galo = storeGalo
-  maze = storeMaze
-  num1 = 0
-  num2 = 0
+  # 지도 만들기
+  @staticmethod
+  def create_maze(storeName, galo, selo):
+    # 지도 초기 규격 만들기
+    storeId = ConvenienceStoreInfo.query.filter_by(storename=storeName).first()
+    ids = storeId.id
 
-  # 지점 리스트 가져오기
-  if strStart == "라면" or strStart == "라면 진열대" or strStart == "라면진열대":
-    num1 = 1
-  elif strStart == "음료" or strStart == "음료 진열대" or strStart == "음료진열대":
-    num1 = 2
-  elif strStart == "과자" or strStart == "과자 진열대" or strStart == "과자진열대":
-    num1 = 3
-  elif strStart == "기타" or strStart == "기타 진열대" or strStart == "기타진열대":
-    num1 = 4
-  elif strStart == "카운터":
-    num1 = 5
-  elif strStart == "입구":
-    num1 = 6
+    storeMaze = [[0] * galo for i in range(selo)]
 
-  if strEnd == "라면" or strEnd == "라면 진열대" or strEnd == "라면진열대":
-    num2 = 1
-  elif strEnd == "음료" or strEnd == "음료 진열대" or strEnd == "음료진열대":
-    num2 = 2
-  elif strEnd == "과자" or strEnd == "과자 진열대" or strEnd == "과자진열대":
-    num2 = 3
-  elif strEnd == "기타" or strEnd == "기타 진열대" or strEnd == "기타진열대":
-    num2 = 4
-  elif strEnd == "카운터":
-    num2 = 5
-  elif strEnd == "입구":
-    num2 = 6
+    storeMap = ConvenienceStoreInfo.query.filter_by(storeinfoid = ids).all()
 
-  # 각 리스트의 값을 활용하여 지점의 x, y좌표를 계산한다.
-  calcullateGalo1 = 0
-  calcullateSelo1 = 0
-  count1 = 0
-  calcullateGalo2 = 0
-  calcullateSelo2 = 0
-  count2 = 0
+    for storeMap in storeMap:
+      row = storeMap.storex
+      col = storeMap.storey
+      sangte = storeMap.storestate
 
-  # 가로, 세로 끼리 각각 총합, 총 개수
-  for i in range(selo):
-    for j in range(galo):
-      if maze[i][j] == num1:
-        calcullateGalo1 += j
-        calcullateSelo1 += i
-        count1 += 1
-      if maze[i][j] == num2:
-        calcullateGalo2 += j
-        calcullateSelo2 += i
-        count2 += 1
+      storeMaze[col][row] = sangte
 
-  # 중간 지점 계산하기.
-  pointGalo1 = int(calcullateGalo1 / count1)  # 출발 지점의 가로 중간
-  pointSelo1 = int(calcullateSelo1 / count1)  # 출발 지점의 세로 중간
-  pointGalo2 = int(calcullateGalo2 / count2)  # 도착 지점의 가로 중간
-  pointSelo2 = int(calcullateSelo2 / count2)  # 도착 지점의 세로 중간
+    return storeMaze
 
-  # 각 지점의 값이 움직임이 가능한 부분인지 확인하기
-  # <<< 가중치 순서 [ 1 우 ][ 2 좌 ][ 3 하 ][ 4 상 ] >>>
-  # 출발 가로 좌표에 대한 검사.
-  if pointGalo1 == galo - 1:
-    pointGalo1 -= 1
-  elif pointGalo1 == 0:
-    pointGalo1 += 1
 
-  # 출발 세로 좌표에 대한 검사
-  if pointSelo1 == selo - 1:
-    pointSelo1 -= 1
-  elif pointSelo1 == 0:
-    pointSelo1 += 1
+  # 출발, 도착 지점 가져오기 (편의점 이름, 지점 선택 번호(출,도 int))
+  # 배열 RETURN -> 0: 출발 가로 / 1: 출발 세로 / 2: 도착 가로 / 3: 도착 세로
+  @staticmethod
+  def get_start_end(startP, endP, storeSelo, storeGalo, storeMaze):
+    strStart = startP
+    strEnd = endP
+    selo = storeSelo
+    galo = storeGalo
+    maze = storeMaze
+    num1 = 0
+    num2 = 0
 
-  # 도착 가로 좌표에 대한 검사
-  if pointGalo2 == galo - 1:
-    pointGalo2 -= 1
-  elif pointGalo2 == 0:
-    pointGalo2 += 1
+    # 지점 리스트 가져오기
+    if strStart == "라면" or strStart == "라면 진열대" or strStart == "라면진열대":
+      num1 = 1
+    elif strStart == "음료" or strStart == "음료 진열대" or strStart == "음료진열대":
+      num1 = 2
+    elif strStart == "과자" or strStart == "과자 진열대" or strStart == "과자진열대":
+      num1 = 3
+    elif strStart == "기타" or strStart == "기타 진열대" or strStart == "기타진열대":
+      num1 = 4
+    elif strStart == "카운터":
+      num1 = 5
+    elif strStart == "입구":
+      num1 = 6
 
-  # 도착 세로 좌표에 대한 검사
-  if pointSelo2 == selo - 1:
-    pointSelo2 -= 1
-  elif pointSelo2 == 0:
-    pointSelo2 += 1
+    if strEnd == "라면" or strEnd == "라면 진열대" or strEnd == "라면진열대":
+      num2 = 1
+    elif strEnd == "음료" or strEnd == "음료 진열대" or strEnd == "음료진열대":
+      num2 = 2
+    elif strEnd == "과자" or strEnd == "과자 진열대" or strEnd == "과자진열대":
+      num2 = 3
+    elif strEnd == "기타" or strEnd == "기타 진열대" or strEnd == "기타진열대":
+      num2 = 4
+    elif strEnd == "카운터":
+      num2 = 5
+    elif strEnd == "입구":
+      num2 = 6
 
-  # 출발지점 좌표 확인하기
-  if maze[pointSelo1][pointGalo1] != 0:
-    if maze[pointSelo1][pointGalo1 + 1] == 0:  # 우.
-      pointGalo1 += 1
-    elif maze[pointSelo1][pointGalo1 - 1] == 0:  # 좌
+    # 각 리스트의 값을 활용하여 지점의 x, y좌표를 계산한다.
+    calcullateGalo1 = 0
+    calcullateSelo1 = 0
+    count1 = 0
+    calcullateGalo2 = 0
+    calcullateSelo2 = 0
+    count2 = 0
+
+    # 가로, 세로 끼리 각각 총합, 총 개수
+    for i in range(selo):
+      for j in range(galo):
+        if maze[i][j] == num1:
+          calcullateGalo1 += j
+          calcullateSelo1 += i
+          count1 += 1
+        if maze[i][j] == num2:
+          calcullateGalo2 += j
+          calcullateSelo2 += i
+          count2 += 1
+
+    # 중간 지점 계산하기.
+    pointGalo1 = int(calcullateGalo1 / count1)  # 출발 지점의 가로 중간
+    pointSelo1 = int(calcullateSelo1 / count1)  # 출발 지점의 세로 중간
+    pointGalo2 = int(calcullateGalo2 / count2)  # 도착 지점의 가로 중간
+    pointSelo2 = int(calcullateSelo2 / count2)  # 도착 지점의 세로 중간
+
+    # 각 지점의 값이 움직임이 가능한 부분인지 확인하기
+    # <<< 가중치 순서 [ 1 우 ][ 2 좌 ][ 3 하 ][ 4 상 ] >>>
+    # 출발 가로 좌표에 대한 검사.
+    if pointGalo1 == galo - 1:
       pointGalo1 -= 1
-    elif maze[pointSelo1 + 1][pointGalo1] == 0:  # 하
-      pointSelo1 += 1
-    elif maze[pointSelo1 - 1][pointGalo1] == 0:  # 상
+    elif pointGalo1 == 0:
+      pointGalo1 += 1
+
+    # 출발 세로 좌표에 대한 검사
+    if pointSelo1 == selo - 1:
       pointSelo1 -= 1
+    elif pointSelo1 == 0:
+      pointSelo1 += 1
 
-  # 도착지점 좌표 확인하기
-  if maze[pointSelo2][pointGalo2] != 0:
-    if maze[pointSelo2][pointGalo2 + 1] == 0:  # 우
-      pointGalo2 += 1
-    elif maze[pointSelo2][pointGalo2 - 1] == 0:  # 좌
+    # 도착 가로 좌표에 대한 검사
+    if pointGalo2 == galo - 1:
       pointGalo2 -= 1
-    elif maze[pointSelo2 + 1][pointGalo1] == 0:  # 하
-      pointSelo2 += 1
-    elif maze[pointSelo2 - 1][pointGalo1] == 0:  # 상
-      pointSelo2 -= 1
+    elif pointGalo2 == 0:
+      pointGalo2 += 1
 
-  arr = []
-  arr.append(pointGalo1)
-  arr.append(pointSelo1)
-  arr.append(pointGalo2)
-  arr.append(pointSelo2)
-  return arr
+    # 도착 세로 좌표에 대한 검사
+    if pointSelo2 == selo - 1:
+      pointSelo2 -= 1
+    elif pointSelo2 == 0:
+      pointSelo2 += 1
+
+    # 출발지점 좌표 확인하기
+    if maze[pointSelo1][pointGalo1] != 0:
+      if maze[pointSelo1][pointGalo1 + 1] == 0:  # 우.
+        pointGalo1 += 1
+      elif maze[pointSelo1][pointGalo1 - 1] == 0:  # 좌
+        pointGalo1 -= 1
+      elif maze[pointSelo1 + 1][pointGalo1] == 0:  # 하
+        pointSelo1 += 1
+      elif maze[pointSelo1 - 1][pointGalo1] == 0:  # 상
+        pointSelo1 -= 1
+
+    # 도착지점 좌표 확인하기
+    if maze[pointSelo2][pointGalo2] != 0:
+      if maze[pointSelo2][pointGalo2 + 1] == 0:  # 우
+        pointGalo2 += 1
+      elif maze[pointSelo2][pointGalo2 - 1] == 0:  # 좌
+        pointGalo2 -= 1
+      elif maze[pointSelo2 + 1][pointGalo1] == 0:  # 하
+        pointSelo2 += 1
+      elif maze[pointSelo2 - 1][pointGalo1] == 0:  # 상
+        pointSelo2 -= 1
+
+    arr = []
+    arr.append(pointGalo1)
+    arr.append(pointSelo1)
+    arr.append(pointGalo2)
+    arr.append(pointSelo2)
+    return arr
--- find_path/view/find_view.py
+++ find_path/view/find_view.py
@@ -1,23 +1,18 @@
-from flask import Flask, Blueprint, request, jsonify
-import sys, os
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-from service import find_service as fc
-
-app = Flask(__name__)
+from flask import Blueprint, request, jsonify
+from find_path.service.find_service import Detectservice
 
 # Blueprint 객체 생성
-jindongMap_bp = Blueprint('path', __name__)
-
+findpath_bp = Blueprint('findpath_bp', __name__)
 
 # 라우트 등록하기
-@jindongMap_bp.route('/find_shortest_path', methods=['POST'])
+@findpath_bp.route('/find_shortest_path', methods=['POST'])
 def find_shortest_path():
           data = request.get_json()  # POST로 전송된 JSON 데이터 받기
           storeName = data['name']
           startP = data['startPoint']
           endP = data['endPoint']
 
-          result = fc.find_shortest_path(storeName, startP, endP)
+          result = Detectservice.find_shortest_path(storeName, startP, endP)
         #   # path 값 = 연산 결과 예시
         #   result = [(1, 8), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7),
         #             (6, 6), (6, 5), (6, 4), (6, 3), (6, 2), (5, 2), (4, 2),
@@ -29,19 +24,28 @@ def find_shortest_path():
 # 실제 연산 부분
 def jindongMap_result(storeName, startP, endP):
           # 지도의 가로, 세로 규격 가져오기
-          size = fc.get_maze_size(storeName)
+          size = Detectservice.get_maze_size(storeName)
           galo = size[0]
           selo = size[1]
 
           # 지도 만들기
-          maze = fc.create_maze(storeName, galo, selo)
+          maze = Detectservice.create_maze(storeName, galo, selo)
 
           # 출발, 도착 지점 가져오기
-          points = fc.get_start_end(startP, endP, selo, galo, maze)
+          points = Detectservice.get_start_end(startP, endP, selo, galo, maze)
           # [0]: 출발 가로 / [1]: 출발 세로 / [2]: 도착 가로 / [3]: 도착 세로
           start = (points[1], points[0])
           end = (points[3], points[2])
 
           # 경로 찾기
-          path = fc.find_shortest_path(maze, start, end)
+          path = Detectservice.find_shortest_path(maze, start, end)
           return path
+
+# test용
+@findpath_bp.route('/connect_dbtest_path', methods=['GET'])
+def dummy():
+        name="미래혁신관"
+        arr = Detectservice.get_maze_size(name)
+        print("galo: ", arr[0])
+        print("selo: ", arr[1])
+        return jsonify(arr)
